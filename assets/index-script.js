274b7cc8c9b0287f3e0f5b869186edb3dce39f43
sahimjbc <jbc.sahim@gmail.com>
2025-12-04 12:22:00 +0600

    Remove commented code and clean up flip card interaction for improved readability

--- assets/index-script.js
+++ assets/index-script.js
@@ -2,7 +2,6 @@ document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
 
-// onclick .front then add class active to .flip-card
 const flipCard = document.querySelector(".flip-card");
 flipCard.addEventListener("click", function () {
   flipCard.classList.add("active");
